367f752c9319d1dc818a0f74440bb1d725fdcb13
adrien.olivencia <adrien.olivencia@simsoft3d.fr>
2019-07-03 09:51:39 +0200

    Add SymbolSource.Service

---
+++ SymbolSource.Service/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymbolSource.Service
+{
+    static class Program
+    {
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new SymbolSource()
+            };
+
+            if (Environment.UserInteractive && System.Diagnostics.Debugger.IsAttached)
+            {
+                // Simule l'exécution des services
+                RunInteractiveServices(ServicesToRun);
+            }
+            else
+            {
+                // Exécute les services normalement
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+
+        static void RunInteractiveServices(ServiceBase[] servicesToRun)
+        {
+            MethodInfo onStartMethod = typeof(ServiceBase).GetMethod("OnStart", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            // Boucle de démarrage des services
+            foreach (ServiceBase service in servicesToRun)
+            {
+                Console.Write("Starting {0} ... ", service.ServiceName);
+                onStartMethod.Invoke(service, new object[] { new string[] { } });
+                Console.WriteLine("Done");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press key to stop...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+
+            MethodInfo onStopMethod = typeof(ServiceBase).GetMethod("OnStop", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            foreach (ServiceBase service in servicesToRun)
+            {
+                Console.Write("Stopping {0} ... ", service.ServiceName);
+                onStopMethod.Invoke(service, null);
+                Console.WriteLine("OK");
+            }
+        }
+    }
+}
---
+++ SymbolSource.Service/SymbolSource.cs
@@ -0,0 +1,79 @@
+﻿using Autofac;
+using SymbolSource.Contract;
+using SymbolSource.Contract.Container;
+using SymbolSource.Contract.Scheduler;
+using SymbolSource.Processor;
+using SymbolSource.Support;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SymbolSource.Service
+{
+    public partial class SymbolSource : ServiceBase
+    {
+        private CancellationTokenSource _stopSource;
+
+        public SymbolSource()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnStart(string[] args)
+        {
+            Trace.Listeners.Add(new ConsoleTraceListener());
+
+            foreach (var assembly in typeof(PackageProcessor).Assembly.GetReferencedAssemblies())
+                Trace.WriteLine(assembly.FullName);
+
+            _stopSource = new CancellationTokenSource();
+
+
+            var configuration = new DefaultConfigurationService();
+            var builder = new ContainerBuilder();
+
+            DefaultContainerBuilder.Register(builder, configuration);
+            SupportContainerBuilder.Register(builder, SupportEnvironment.WebJob);
+            PackageProcessorContainerBuilder.Register(builder);
+
+            var container = builder.Build();
+
+            var support = container.Resolve<ISupportConfiguration>();
+
+            var scheduler = container.Resolve<ISchedulerService>();
+            Task.Run(() =>
+            {
+                int i = 0;
+                while (!_stopSource.IsCancellationRequested)
+                {
+                    try
+                    {
+                        scheduler.ListenAndProcess(_stopSource.Token);
+                    }
+                    catch
+                    {
+                        i++;
+                        if(i > 10)
+                        {
+                            Stop();
+                        }
+                    }
+                }
+            });
+
+        }
+
+        protected override void OnStop()
+        {
+            _stopSource.Cancel();
+        }
+    }
+}
