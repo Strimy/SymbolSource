09deea0950db661ba5cacd4319edcd924fb29700
adrien.olivencia <adrien.olivencia@simsoft3d.fr>
2019-07-03 09:52:42 +0200

    Fix FileSchedulerService not being able access shared file

--- SymbolSource.Contract/Scheduler/FileSchedulerService.cs
+++ SymbolSource.Contract/Scheduler/FileSchedulerService.cs
@@ -46,13 +46,39 @@ namespace SymbolSource.Contract.Scheduler
 
         private async void Process(string file)
         {
-            var message = JsonConvert.DeserializeObject<PackageMessage>(File.ReadAllText(file));
-            Trace.TraceInformation("Processing message {0}", message);
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    using (var fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Delete))
+                    {
+                        using (StreamReader sr = new StreamReader(fs))
+                        {
+                            var text = sr.ReadToEnd();
+                            var message = JsonConvert.DeserializeObject<PackageMessage>(text);
+                            Trace.TraceInformation("Processing message {0}", message);
 
-            await processor.Value.Process(message);
+                            await processor.Value.Process(message);
 
-            Trace.TraceInformation("Removing signal for message {0}", message);
+                            Trace.TraceInformation("Removing signal for message {0}", message);
+                        }
+                    }
+                    break;
+                }
+                catch(Exception e)
+                {
+                    Trace.TraceInformation("Failed : {0}", e.ToString());
+                    Thread.Sleep(100);
+                    if(i == 2)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+
+            }
+
             File.Delete(file);
+
         }
     }
 
